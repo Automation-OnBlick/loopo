fb47932ece4c5052a4876317d6f96c77d5ea2181
Automation-OnBlick <joel@onblick.com>
2021-01-04 18:16:33 +0530

    loopo

---
+++ Clientliker/js/bugreport.js
@@ -0,0 +1,41 @@
+const $ = require('jquery');
+const fs = require('fs');
+const path = require('path');
+const {remote, app} = require('electron');
+const { ajax } = require('jquery');
+var appId;  
+
+$(document).ready(function (e) {
+  //add fs get file here 
+  fs.readFile(remote.app.getPath('userData')+'/applicationData/user.joel', (error,data) =>{
+    if(!error){
+      appId = data;
+    }
+  });
+      $('#reportForm').on('submit',(function(e) {
+          e.preventDefault();
+          var formData = new FormData(this);
+          $('#reportForm').hide();
+          console.log(formData);
+          $.ajax({
+              type:'POST',
+              url: $(this).attr('action')+'/'+appId,
+              data:formData,
+              cache:false,
+              contentType: false,
+              processData: false,
+              success:function(response){
+                  $('#reportForm').html('<h3 class="nerko" style="color:#fff" >Thank you for the feedback.</h3>');
+                  $('#reportForm').show();
+                  setTimeout(()=>{
+                    currentWindow = remote.getCurrentWindow();
+                    currentWindow.close();
+                    currentWindow = null;
+                  },5000);
+              },
+              error: function(data){
+                  console.log('Error : ',data);
+              }
+          });
+      }));
+  });
